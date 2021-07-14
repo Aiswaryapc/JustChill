8e4709ac46fc2167a968f242f58c63ce0857a78f
Aiswaryapc <aiswaryapchandran3@gmail.com>
2021-07-14 23:56:20 +0530

    first

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,20 +2,21 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter/cupertino.dart';
+import 'package:just_chill/top_navigation.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color:Colors.black26 ,
+      color:bg ,
       child: Container(
-        color:Colors.black26 ,
+        color:bg ,
         child: Column(
 
           children: [
             Container(
               height: 100,
-              color:  appbar,
+              color:  bg,
               child: Row(
                 children: [
                   Padding( padding: const EdgeInsets.only(left: 6,top: 40 ),
@@ -23,23 +24,28 @@ class HomeScreen extends StatelessWidget {
                       child: Text("My Music",style: TextStyle(
                           fontSize: 35,
                           fontWeight:  FontWeight.w900,
-                          fontFamily: 'Montserrat' ,
+                          fontFamily: 'Mellgatha' ,
                           foreground: Paint()
                             ..shader = const LinearGradient(
                               colors: <Color>[
+                                appbar,
                                 Colors.white,
-                                Colors.yellow,
                                 Colors.yellow
                               ],
                             ).createShader(const Rect.fromLTWH(100, 100, 200, 100))
 
                       ) ),
                     ),
-                  )
+                  ),
+                  Expanded(child: Container()),
+                  Padding(padding: const EdgeInsets.only(top: 40 ),
+                      child: Icon(Icons.search,color: appbar,size: 30,)),
+                  Padding(padding: const EdgeInsets.only(right: 6,top: 40 ),
+                      child: Icon(Icons.settings,color: appbar,size: 30,))
                 ],
               ),
-
             ),
+            TopNavigation(),
 
           ],
         ),
@@ -49,4 +55,6 @@ class HomeScreen extends StatelessWidget {
 }
 
 // ignore: use_full_hex_values_for_flutter_colors
-const appbar = Color(0xFFb00000);
+const appbar = Color(0xFF9d0505);
+const bg = Color(0xFF181c25);
+
---
+++ lib/top_navigation.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TopNavigation extends StatefulWidget {
+  const TopNavigation({Key? key}) : super(key: key);
+
+  @override
+  _TopNavigationState createState() => _TopNavigationState();
+}
+
+class _TopNavigationState extends State<TopNavigation> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Container();
+
+
+  }
+}
